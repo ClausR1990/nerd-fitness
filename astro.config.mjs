81cd549e68957f96e402cf5cbe21f1032b9a1794
ClausR1990 <claus.reinhold@gmail.com>
2023-05-29 09:40:57 +0200

    added base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://clausr1990.github.io",
+  base: "/nerd-fitness",
   integrations: [tailwind()],
 });
